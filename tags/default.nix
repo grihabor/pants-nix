8f0aad28e5f92a1242072e9349dcaf8ab3517e6b
Borodin Gregory <grihabor@gmail.com>
2024-05-14 17:37:03 +0200

    Add release_2.21.0a0

--- tags/default.nix
+++ tags/default.nix
@@ -1,4 +1,5 @@
 {pkgs}: {
   "release_2.20.0" = pkgs.callPackage ./release_2.20.0 {};
   "release_2.20.1" = pkgs.callPackage ./release_2.20.1 {};
+  "release_2.21.0a0" = pkgs.callPackage ./release_2.21.0a0 {};
 }
