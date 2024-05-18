0ada01397c551c7df8898b0fd967996ff05fca39
Borodin Gregory <grihabor@gmail.com>
2024-05-18 19:10:05 +0200

    Clean up gen.py

--- gen.py
+++ gen.py
@@ -25,89 +25,8 @@ import tomllib
 logger = logging.getLogger(__name__)
 
 
-def download_file(*, url: str, output_path: str):
-    logger.info("downloading: %s", url)
-    r = requests.get(url)
-    r.raise_for_status()
-    Path(output_path).write_text(r.text)
-
-
-def download_toml(*, url: str):
-    logger.info("downloading toml: %s", url)
-    r = requests.get(url)
-    r.raise_for_status()
-    result = tomllib.load(BytesIO(r.content))
-    logger.info("got toml: %s", result)
-    return result
-
-
 def _run(command: str) -> bytes:
     return sp.check_output(shlex.split(command))
-
-
-@dataclass(frozen=True)
-class Repo:
-    path: Path
-    url: str
-
-    @classmethod
-    def default(cls) -> Repo:
-        return Repo(
-            url="https://github.com/pantsbuild/pants.git",
-            path=Path(os.environ["HOME"]) / ".cache" / "pants-nix" / "pants",
-        )
-
-    def fetch(self) -> None:
-        self.path.parent.mkdir(parents=True, exist_ok=True)
-        if not self.path.exists():
-            _run(f"git clone {self.url} {self.path}")
-
-        _run(f"git -C {self.path} fetch origin")
-
-    def read_file(self, path: str, tag: str) -> str:
-        _run(f"git -C {self.path} checkout {tag}")
-        return (self.path / path).read_text()
-
-    def tag_hash(self, tag: str) -> str:
-        with TemporaryDirectory() as d:
-            archive = Path(d) / "archive.tar.gz"
-            _run(f"git -C {self.path} archive -o {archive} {tag}")
-            path = Path(d) / f"{tag}.tar.gz"
-            path.mkdir()
-            _run(f"tar -xf {archive} -C {path}")
-            _run(f"find {path} -maxdepth 1")
-            result = _run(f"nix-hash --type sha256 --base32 --sri {path}")
-
-        return result.decode(encoding="utf-8").strip()
-
-    def list_versions(self) -> list[Version]:
-        _run(f"git -C {self.path} checkout origin/main")
-        lines = _run(f"git -C {self.path} tag --list release_*").decode("utf-8").splitlines()
-        versions = [Version.from_tag(line.strip()) for line in lines]
-        versions.sort()
-        return versions
-
-
-class Version(NamedTuple):
-    major: int
-    minor: int
-    micro: int
-    other: str
-
-    @classmethod
-    def from_tag(cls, tag: str) -> Version:
-        regex = r"^release_([0-9]+).([0-9]+).([0-9]+)(.*)$"
-        match = re.match(regex, tag)
-        assert match, tag
-        return Version(
-            major=int(match.group(1)),
-            minor=int(match.group(2)),
-            micro=int(match.group(3)),
-            other=str(match.group(4)),
-        )
-
-    def __format__(self, __format_spec: str) -> str:
-        return f"{self.major}.{self.minor}.{self.micro}{self.other}"
 
 
 def main():
@@ -193,6 +112,71 @@ def _generate_list_of_packages():
             tag = f"release_{v}"
             f.write(f'  "{tag}" = pkgs.callPackage ./{tag} {{}};\n')
         f.write("}\n")
+
+
+@dataclass(frozen=True)
+class Repo:
+    path: Path
+    url: str
+
+    @classmethod
+    def default(cls) -> Repo:
+        return Repo(
+            url="https://github.com/pantsbuild/pants.git",
+            path=Path(os.environ["HOME"]) / ".cache" / "pants-nix" / "pants",
+        )
+
+    def fetch(self) -> None:
+        self.path.parent.mkdir(parents=True, exist_ok=True)
+        if not self.path.exists():
+            _run(f"git clone {self.url} {self.path}")
+
+        _run(f"git -C {self.path} fetch origin")
+
+    def read_file(self, path: str, tag: str) -> str:
+        _run(f"git -C {self.path} checkout {tag}")
+        return (self.path / path).read_text()
+
+    def tag_hash(self, tag: str) -> str:
+        with TemporaryDirectory() as d:
+            archive = Path(d) / "archive.tar.gz"
+            _run(f"git -C {self.path} archive -o {archive} {tag}")
+            path = Path(d) / f"{tag}.tar.gz"
+            path.mkdir()
+            _run(f"tar -xf {archive} -C {path}")
+            _run(f"find {path} -maxdepth 1")
+            result = _run(f"nix-hash --type sha256 --base32 --sri {path}")
+
+        return result.decode(encoding="utf-8").strip()
+
+    def list_versions(self) -> list[Version]:
+        _run(f"git -C {self.path} checkout origin/main")
+        lines = _run(f"git -C {self.path} tag --list release_*").decode("utf-8").splitlines()
+        versions = [Version.from_tag(line.strip()) for line in lines]
+        versions.sort()
+        return versions
+
+
+class Version(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    other: str
+
+    @classmethod
+    def from_tag(cls, tag: str) -> Version:
+        regex = r"^release_([0-9]+).([0-9]+).([0-9]+)(.*)$"
+        match = re.match(regex, tag)
+        assert match, tag
+        return Version(
+            major=int(match.group(1)),
+            minor=int(match.group(2)),
+            micro=int(match.group(3)),
+            other=str(match.group(4)),
+        )
+
+    def __format__(self, __format_spec: str) -> str:
+        return f"{self.major}.{self.minor}.{self.micro}{self.other}"
 
 
 if __name__ == "__main__":
