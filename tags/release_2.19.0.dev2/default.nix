ce2cb5ad8beb40f57de62498c5d7e3db80b4f0f4
Gregory Borodin <grihabor@gmail.com>
2024-10-01 18:50:03 +0200

    ./gen.py all (#3)

--- tags/release_2.19.0.dev2/default.nix
+++ tags/release_2.19.0.dev2/default.nix
@@ -12,12 +12,14 @@ let
     # https://raw.githubusercontent.com/pantsbuild/pants/release_2.19.0.dev2/src/rust/engine/Cargo.lock
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "console-0.15.7" = "sha256-EsUtBySVj2aoGOPBteDKCY7PCehJoqEJXpjOyQlpCf4=";
       "deepsize-0.2.0" = "sha256-E73xdzYfpJASps3yz6sjL48Kimy44F2LvxndWzgV3dU=";
+      "deepsize_derive-0.1.2" = "sha256-E73xdzYfpJASps3yz6sjL48Kimy44F2LvxndWzgV3dU=";
       "globset-0.4.10" = "sha256-1ucpIHxISBqjvKBAea7o2wSddWiIQr6tBiInk4kg0P0=";
-      "indicatif-0.17.7" = "sha256-GxQM+y5zL1KW5HmN9UcuS3xNNiZC8neMCyGIoOMleLs=";
+      "ignore-0.4.20" = "sha256-1ucpIHxISBqjvKBAea7o2wSddWiIQr6tBiInk4kg0P0=";
       "lmdb-rkv-0.14.0" = "sha256-yj0+3wRQkAyp5EYOe2WQeUt1D/3cXZK0XrH6qcxhaWw=";
+      "lmdb-rkv-sys-0.11.0" = "sha256-c9lKJuE74Xp/sIwSFXFsl2EKffY3oC7Prnglt6p1Ah0=";
       "notify-5.0.0-pre.15" = "sha256-LG6e3dSIqQcHbNA/uYSVJwn/vgcAH0noHK4x3QQdqVI=";
+      "opendal-0.40.0" = "sha256-sLRtFPHKaZuxF2S3djxkxNr84LFRqcDjvsVr2eMDKsM=";
       "prodash-16.0.0" = "sha256-Dkn4BmsF1SnSDAoqW5QkjdzGHEq41y7S20Q/DkRCpVQ=";
     };
   };
